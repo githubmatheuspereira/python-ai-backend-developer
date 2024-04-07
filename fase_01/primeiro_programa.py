791eb2e5f2ad4f446c6966982103859badbe0fb8
Matheus Pereira <contatomatheuspereirasilva@gmail.com>
2024-04-06 23:05:05 -0300

    upload new files

---
+++ fase_01/primeiro_programa.py
@@ -0,0 +1,11 @@
+"""
+Objetivo geral:
+Fazer o primeiro programa.
+
+Computadores "lêem receitas".
+Programar consiste em informar ao computador uma sequência de rotinas que devem ser processadas, igual a uma receita de bolo.
+
+O Python utiliza a extensão .py
+"""
+
+print("Hello World!!!")
---
+++ fase_01/tipos_de_dados.py
@@ -0,0 +1,35 @@
+"""
+Objetivo Geral:
+Tipos de dados em Python
+
+Percurso
+O que são tipos?
+Tipos numérios.
+Booleanos e Strings
+
+O que são tipos?
+	Os tipos servem para definir as caracteristicas e comportamentos de um valor (objeto) para o interpretador.
+
+Tipos.
+	int - são números inteiros e possuem precisão ilimitada. Exemplos: 1, 10, 256, -64, 2048...
+	float - são números com ponto flutuante. Representa numeros racionais como: 3.14, 2.045, -98.64...
+	bool - é usado para representar o verdadeiro ou falso. Qualquer número diferente de 0, será verdaderio. Exemplo: 15 = verdadeiro, 0.987 = verdadeiro, 0 = falso.
+	
+	str - são usados para representar valores alfanuméricos. Exemplos: "Python",'Py7h0n','''pyThoN'''...
+
+"""
+
+#int.
+print(1 + 10)
+
+#float.
+print(1.5 + 2 + 0.5)
+
+#bool.
+print(True)
+print(False)
+
+#str.
+print("Python")
+print('Py7hon')
+print("""pY7hOn""")
