fc2912367e9a83caba7556f0efed8c7686cb5783
Matheus Pereira Silva <contatomatheuspereirasilva@gmail.com>
2024-04-07 17:45:40 -0300

    upload new file

---
+++ fase_01/variaveis_e_constantes.py
@@ -0,0 +1,12 @@
+"""
+Objetivo Geral:
+Entender o que são e como utilizar variáveis e constantes.
+
+Etapa 1 - O que são variáveis constantes?
+Etapa 2 - Boas práticas.
+
+Variáveis
+Em linguagens de programação, variáveis são valores que podem sofrer alterações no decorrer da execução do programa.
+
+
+"""
