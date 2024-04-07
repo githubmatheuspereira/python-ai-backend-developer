c8676be31ad01fb6fa6ea0c24d268ce86d05c4fa
Matheus Pereira <contatomatheuspereirasilva@gmail.com>
2024-04-07 19:47:22 -0300

    upload new file

---
+++ fase_01/conhecendo_a_linguagem.py
@@ -0,0 +1,18 @@
+"""
+Objetivo Geral:
+Aprender a converter os tipos das variáveis.
+
+Etapa 1 - Convertendo tipos
+
+Convertendo tipos
+Em alguns momentos será necessário converter o tipo de uma variável para manipular de forma diferente. Por exemplo:
+Variáveis do tipo string, que armazenam números e precisamos fazer alguma operação matemática com esse valor.
+
+
+"""
+
+valor = 3.1415
+
+print(int(valor), type(int(valor)))
+print(float(valor), type(float(valor)))
+print(str(valor), type(str(valor)))
