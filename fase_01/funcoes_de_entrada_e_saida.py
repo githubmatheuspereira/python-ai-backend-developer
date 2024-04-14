f3bbef887b6ad66910773c954d4bae4a8dad8898
Matheus Pereira <contatomatheuspereirasilva@gmail.com>
2024-04-14 15:40:19 -0300

    modified file

--- fase_01/funcoes_de_entrada_e_saida.py
+++ fase_01/funcoes_de_entrada_e_saida.py
@@ -9,7 +9,7 @@ Função input:
 É ultilizada quando queremos ler dados da entrada padrão (teclado). Ela recebe um argumento do tipo String, que é exibido para o usuário na saída padrão (tela). A função lê a entrada, converte para string e retorna o valor.
 
 Função print:
-É ultilizada quando queremos exibir dados na saída padrão (tela). Ela recebe um argumento obrigatório do tipo varargs de objetos e 4 argumentos opcionais (sep, end, file e flush). Todos os objetos são convertidos para string, separados por sep e terminados por end. A string final é exibida para o usuário.
+É utilizada quando queremos exibir dados na saída padrão (tela). Ela recebe um argumento obrigatório do tipo varargs de objetos e 4 argumentos opcionais (sep, end, file e flush). Todos os objetos são convertidos para string, separados por sep e terminados por end. A string final é exibida para o usuário.
 
 """
 
@@ -18,3 +18,4 @@ idade = input("Informe a sua idade: ")
 
 print(nome, idade)
 print(nome, idade, end="...\n")
+print(nome, idade, sep="_")
